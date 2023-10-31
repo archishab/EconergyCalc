790ee8aedcf7d0a9d3a25c03cff87c60140f6f09
Archisha Bhattacharya <aba406@uregina.ca>
2023-10-31 09:25:27 -0600

    add auth.js functionality

--- 02. Project Development/econergycalc/backend/index.js
+++ 02. Project Development/econergycalc/backend/index.js
@@ -10,9 +10,11 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(express.json())
+
+// Available routes
+app.use('/api/auth', require('./routes/auth.js'))
+app.use('/api/appliances', require('./routes/appliances.js'))
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)

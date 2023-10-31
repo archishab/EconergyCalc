248cd71d0877577652e1ec42437df07230a47c9f
Archisha Bhattacharya <aba406@uregina.ca>
2023-10-31 08:42:34 -0600

    Create User and Appliance Schema

---
+++ 02. Project Development/econergycalc/backend/models/Appliance.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const applianceSchema = new Schema({
+    applianceType: {
+        type: String,
+        required: true
+    },
+    applianceName: {
+        type: String,
+        required: true
+    },
+    powerRating: {
+        type: Number,
+        required: true
+    },
+    usageDuration: {
+        type: Number,
+        required: true
+    },
+    quantity: {
+        type: Number,
+        required: true
+    },
+  });
+
+module.exports = mongoose.model('notes', applianceSchema);

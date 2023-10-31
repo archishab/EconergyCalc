6cfb7381f5021ccb9362a6fff5a9fd593c9f1349
Archisha Bhattacharya <aba406@uregina.ca>
2023-10-31 09:54:36 -0600

    Add error handling for user authentication

--- 02. Project Development/econergycalc/backend/routes/auth.js
+++ 02. Project Development/econergycalc/backend/routes/auth.js
@@ -1,14 +1,26 @@
 const express = require('express');
+const { body, query, validationResult } = require('express-validator');
 const User = require('../models/User');
 const router = express.Router();
 
 
 // Create a user using: POST "/api/auth/"
-router.post('/', (req, res)=>{
-    console.log(req.body);
-    const user = User(req.body);
-    user.save();
-    res.send(req.body)
-})
+router.post('/', [
+    body('username', 'Username cannot be empty').isLength({ min: 1 }),
+    body('email', 'Email must be valid').isEmail(),
+    body('password', 'Password must be more than 5 characters').isLength({ min: 5 })
+],(req, res)=>{
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    };
+    User.create({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+    }).then(user => res.json(user))
+    .catch(err=>{console.log(err)
+    res.json({error: 'Please enter a unique value for email and username', message: err.message})});
+});
 
 module.exports = router;

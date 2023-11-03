adfc1f78065c507a19a4f8a823af7fc1ec5c69c5
Archisha Bhattacharya <aba406@uregina.ca>
2023-11-03 17:04:01 -0600

    Update navbar

--- 02. Project Development/econergycalc/src/components/Home.js
+++ 02. Project Development/econergycalc/src/components/Home.js
@@ -1,31 +1,52 @@
 import React from "react";
 import BigLogo from "../assets/BigLogo.png";
-import FullLogo from "../assets/FullLogo.png";
 
 import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
-    <div className="container text-center">
-      <img src={FullLogo} />
-      <div class="d-grid gap-2 col-6 mx-auto">
-        <Link
-          to="/login"
-          className="btn btn-warning btn-lg"
-          tabIndex="1"
-          role="button"
-        >
-          Login
-        </Link>
-        <Link
-          to="/signup"
-          className="btn btn-warning btn-lg"
-          tabIndex="1"
-          role="button"
-        >
-          Sign Up
-        </Link>
+    <>
+      <div class="container my-5">
+        <div class="row p-4 pb-0 pe-lg-0 pt-lg-5 align-items-center rounded-3 border shadow-lg">
+          <div class="col-lg-4 p-0 overflow-hidden">
+            <img
+              class="d-block mx-auto"
+              src={BigLogo}
+              alt="EconergyCalc"
+              width="300"
+            />
+          </div>
+          <div class="col-lg-7 p-3 p-lg-5 pt-lg-3">
+            <h1 class="display-4 fw-bold lh-1 text-body-emphasis">
+              EconergyCalc
+            </h1>
+            <p class="lead">
+              Quickly design and customize responsive mobile-first sites with
+              Bootstrap, the world’s most popular front-end open source toolkit,
+              featuring Sass variables and mixins, responsive grid system,
+              extensive prebuilt components, and powerful JavaScript plugins.
+            </p>
+            <div class="d-grid gap-2 d-md-flex justify-content-md-start mb-4 mb-lg-3">
+              <Link
+                to="/login"
+                className="btn btn-warning btn-lg"
+                tabIndex="1"
+                role="button"
+              >
+                Login
+              </Link>
+              <Link
+                to="/signup"
+                className="btn btn-outline-warning btn-lg"
+                tabIndex="1"
+                role="button"
+              >
+                Sign Up
+              </Link>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
--- 02. Project Development/econergycalc/src/components/Navbar.js
+++ 02. Project Development/econergycalc/src/components/Navbar.js
@@ -1,61 +1,122 @@
-import LogoIcon from '../assets/LogoIcon.png';
-import Logo from '../assets/Logo.png';
-import React from 'react';
-import PropTypes from 'prop-types';
-import {
-  Link
-} from "react-router-dom";
-
+import LogoIcon from "../assets/LogoIcon.png";
+import FullLogo from "../assets/FullLogo.png";
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <nav className="navbar bg-body-tertiary ">
+    <>
+      <nav className="navbar bg-body-tertiary d-flex">
         <div className="container-fluid">
-          <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="offcanvas"
+            data-bs-target="#offcanvasNavbar"
+            aria-controls="offcanvasNavbar"
+            aria-label="Toggle navigation"
+          >
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="offcanvas offcanvas-start" tabIndex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-            <div className="offcanvas-header">
-              <a className="navbar-brand" href="/">
-                <img src={Logo} alt="Bootstrap" height="40" />
-              </a>
+          <div
+            className="offcanvas offcanvas-start"
+            tabIndex="-1"
+            id="offcanvasNavbar"
+            aria-labelledby="offcanvasNavbarLabel"
+          >
+            <div class="full-height d-flex flex-column flex-shrink-0 p-3 bg-body-tertiary ">
+              < div className="">
+              <Link className="navbar-brand" to="/">
+              <img src={FullLogo} alt="Bootstrap" width="300" />
+              </Link>
               <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-            </div>
-            <div className="offcanvas-body">
-              <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
-                <li className="nav-item">
-                  <Link className="nav-link active" aria-current="page" to="/dashboard">Dashboard</Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/add">Add Appliance</Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/recommendations">Recommendations</Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/">Forum</Link>
-                </li>
-                <li className="nav-item dropdown">
-                  <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    Username
+              </div> 
+              <hr />
+              <ul class="nav nav-pills flex-column mb-auto">
+                <li class="nav-item">
+                  <a href="#" class="nav-link active" aria-current="page">
+                    <svg class="bi pe-none me-2" width="16" height="16"></svg>
+                    Home
                   </a>
-                  <ul className="dropdown-menu">
-                    <li><a className="dropdown-item" href="/">Account Settings</a></li>
-                    <li>
-                      <hr className="dropdown-divider" />
-                    </li>
-                    <li><a className="dropdown-item" href="/">Logout</a></li>
-                  </ul>
+                </li>
+                <li>
+                  <a href="#" class="nav-link link-body-emphasis">
+                    <svg class="bi pe-none me-2" width="16" height="16"></svg>
+                    Dashboard
+                  </a>
+                </li>
+                <li>
+                  <a href="#" class="nav-link link-body-emphasis">
+                    <svg class="bi pe-none me-2" width="16" height="16"></svg>
+                    Orders
+                  </a>
+                </li>
+                <li>
+                  <a href="#" class="nav-link link-body-emphasis">
+                    <svg class="bi pe-none me-2" width="16" height="16"></svg>
+                    Products
+                  </a>
+                </li>
+                <li>
+                  <a href="#" class="nav-link link-body-emphasis">
+                    <svg class="bi pe-none me-2" width="16" height="16"></svg>
+                    Customers
+                  </a>
                 </li>
               </ul>
+              <hr />
+              <div class="dropdown">
+                <a
+                  href="#"
+                  class="d-flex align-items-center link-body-emphasis text-decoration-none dropdown-toggle"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  <img
+                    src="https://github.com/mdo.png"
+                    alt=""
+                    width="32"
+                    height="32"
+                    class="rounded-circle me-2"
+                  />
+                  <strong>mdo</strong>
+                </a>
+                <ul class="dropdown-menu text-small shadow">
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      New project...
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      Settings
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      Profile
+                    </a>
+                  </li>
+                  <li>
+                    <hr class="dropdown-divider" />
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      Sign out
+                    </a>
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
           <a className="navbar-brand" href="/">
-            <img src={LogoIcon} alt="Bootstrap" width="50" />
+            <img src={LogoIcon} alt="Bootstrap" width="40" />
           </a>
         </div>
       </nav>
-  )
+    </>
+  );
 }
 
-Navbar.propTypes = {title: PropTypes.string}
+Navbar.propTypes = { title: PropTypes.string };
